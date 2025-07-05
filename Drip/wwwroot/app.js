3e701fb361d819014e5529990ec7a3d70b86d552
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-07-05 11:22:21 -0400

    Implement dark mode and sidebar toggle functionality; refactor related components and styles for improved layout

---
+++ Drip/wwwroot/app.js
@@ -0,0 +1,13 @@
+﻿window.toggleDarkMode = () => {
+  document.documentElement.classList.toggle('dark');
+};
+
+window.toggleSidebar = () => {
+  const width = document.documentElement.clientWidth;
+  
+  if (width < 992) {
+    document.documentElement.classList.toggle('sidebar-open-mobile');
+  } else {
+    document.documentElement.classList.toggle('sidebar-closed-desktop');
+  }
+};
