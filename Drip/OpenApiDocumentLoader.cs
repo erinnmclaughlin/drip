4da49e1b79dc777dad476925c18ee08e83732165
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-07-05 12:09:54 -0400

    Add loading component and OpenAPI document loader; enhance layout and styles for improved user experience

---
+++ Drip/OpenApiDocumentLoader.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Reader;
+
+namespace Drip;
+
+public sealed class OpenApiDocumentLoader(HttpClient httpClient)
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<OpenApiDocument?> LoadLocalDocumentAsync(string fileName = "star-trek.json")
+    {
+        var reader = new OpenApiJsonReader();
+        await using var stream = await _httpClient.GetStreamAsync(fileName);
+        var (document, _) = await reader.ReadAsync(stream, new OpenApiReaderSettings());
+        return document;
+    }
+}
