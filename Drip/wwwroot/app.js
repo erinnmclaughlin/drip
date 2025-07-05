14703e5c2099c7a851bdd8b3d17dfe28371bc31b
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-07-05 11:34:15 -0400

    Implement theme persistence and update dark mode toggle; enhance UI with conditional icons

--- Drip/wwwroot/app.js
+++ Drip/wwwroot/app.js
@@ -1,5 +1,9 @@
 ﻿window.toggleDarkMode = () => {
-  document.documentElement.classList.toggle('dark');
+  if (document.documentElement.classList.toggle('dark')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 };
 
 window.toggleSidebar = () => {
@@ -11,3 +15,17 @@ window.toggleSidebar = () => {
     document.documentElement.classList.toggle('sidebar-closed-desktop');
   }
 };
+
+// On load, set theme from localStorage
+(function () {
+  let theme = localStorage.getItem('theme');
+  if (!theme) {
+    // get preferred theme
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    theme = prefersDark ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+  }
+  if (theme === 'dark') {
+    document.documentElement.classList.add('dark');
+  }
+})();
